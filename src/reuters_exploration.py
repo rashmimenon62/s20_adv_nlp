db40317bab4313e175fe2e7e8a7d51bb0e06349e
eason9 <easoncharles9@gmail.com>
2020-02-04 17:56:37 -0500

    update

--- src/reuters_exploration.py
+++ src/reuters_exploration.py
@@ -13,5 +13,14 @@ for i in reuters.categories():
 
 d = {k: v for k, v in sorted(d.items(), key=lambda item: item[1], reverse=True)}
 
+len(reuters.categories())
 cat = reuters.fileids(['acq'])[0]
 reuters.raw(cat)
+
+# 2/4/2020
+# Look for seed words and reasonable extraction patterns
+# Good seed word: A seed word is some word that is generally related to this type of event.
+# HW: Come in with 10 words - pull the entire sentence with seed words and doc ids.
+# Think about semantic categories you are identifying.
+
+# Next week: talk through the scoring methods
