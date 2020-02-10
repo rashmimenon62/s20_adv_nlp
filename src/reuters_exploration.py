cb1cbeccad6cd83c50f39eb88047b15a054828b1
eason9 <easoncharles9@gmail.com>
2020-02-10 18:15:45 -0500

    update

--- src/reuters_exploration.py
+++ src/reuters_exploration.py
@@ -4,8 +4,12 @@ Created on Mon Feb  3 20:51:20 2020
 
 @author: Garrett
 """
-
+#%% Packages
+import spacy
 from nltk.corpus import reuters
+en = spacy.load("en_core_web_lg")
+
+#%% Reuters exploration
 reuters.readme()
 d = {}
 for i in reuters.categories():
@@ -14,8 +18,8 @@ for i in reuters.categories():
 d = {k: v for k, v in sorted(d.items(), key=lambda item: item[1], reverse=True)}
 
 len(reuters.categories())
-cat = reuters.fileids(['acq'])[0]
-reuters.raw(cat)
+txt = reuters.fileids(['acq'])[0]
+print(reuters.raw(txt))
 
 # 2/4/2020
 # Look for seed words and reasonable extraction patterns
@@ -26,3 +30,40 @@ reuters.raw(cat)
 # Setup SQL alchemy
 
 # Next week: talk through the scoring methods
+
+#%% Using Model
+txt = reuters.raw(reuters.fileids(['acq'])[0])
+print(txt)
+
+
+processed = en(txt)
+
+for token in processed:
+    print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
+            token.shape_, token.is_alpha, token.is_stop)
+    
+for np in processed.noun_chunks:
+    print(np, np.root.dep_)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
